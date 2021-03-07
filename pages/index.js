37878c3c723f470d7e0061272c1a49bd6a98da45
lixinghang12 <54225231+lixinghang12@users.noreply.github.com>
2021-03-07 17:20:14 +0800

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -37,7 +37,7 @@ export default function Home() {
           
         </p>
         
-        <img src="/framework.png"/ className={styles.img}>
+        <img src="/framework.png" className={styles.img} />
         
       </main>
     
