79f2ec158cc7941af1ec3ea4c65669ebeb162dcb
lixinghang12 <54225231+lixinghang12@users.noreply.github.com>
2021-03-07 18:37:31 +0800

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -13,7 +13,10 @@ export default function Home() {
         <h1 className={styles.title}>
           Embodied Semantic Scene Graph Generation
         </h1>
-
+        
+        <h2 className={styles.title}>
+          Abstract
+        </h2>
         <p className={styles.description}>
           &nbsp;&nbsp;&nbsp;&nbsp;Semantic scene graph provides an effective way for
           intelligent agents to better understand the environment and it
@@ -37,19 +40,31 @@ export default function Home() {
           
         </p>
         
+        
         <p className={styles.caption}>
           Fig.1 The proposed framework of embodied semantic scene graph generation.
         </p>
         
         <img src="/framework.png" className={styles.img} />
+        <p className={styles.description}>
           
+          &nbsp;&nbsp;&nbsp;&nbsp;As shown in fig.1, At time instant t, after taking one action, the agent comes to a new viewpoint and the
+          RGB and depth frames are captured. With the RGB and depth frames, the corresponding semantic point cloud is generated, which is then used to update the
+          global point cloud and global semantic scene graph at time instant t. With the current observed RGB frame, current local semantic scene graph and action
+          type at time instant t, a new action is generated for the agent to further explore the environment. This process iterates until a stop action is obtained.
+          Our framework 
+        </p>
+    
         <p className={styles.caption}>
           Vid.1 The introduction video of the generation of demonstrating tracks.
         </p>
-        <video poster="/framework.png" controls className={styles.img}>
+        
+        <video poster="/track_v2.png" controls className={styles.img}>
           <source src="demonstrate_track.mp4" type='video/mp4; codecs="avc1.4D401E, mp4a.40.2"' />
           <p>This is fallback content</p>
         </video>
+        
+        
         
       </main>
     
